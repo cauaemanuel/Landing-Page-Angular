d70f6a8806417d9685039d70c3c407970dc496f8
Caua emanuel <emanuellcaua8@gmail.com>
2025-05-16 15:30:08 -0300

    add layout breno

---
+++ src/app/components/description-me/description-me.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'description-me',
+  imports: [NgOptimizedImage],
+  templateUrl: './description-me.component.html',
+  styleUrl: './description-me.component.scss'
+})
+export class DescriptionMeComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
+import { DescriptionMeComponent } from "../description-me/description-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent, DescriptionMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
